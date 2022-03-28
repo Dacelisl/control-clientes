c350398f53d03b487c19fa5995c7871648aea808
Dacelisl <hero055@gmail.com>
2022-03-28 11:21:07 -0500

    base app-controlClientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './components/cabecero/cabecero.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { EditarComponent } from './components/editar/editar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { TableroComponent } from './components/tablero/tablero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    ClientesComponent,
+    ConfiguracionComponent,
+    EditarComponent,
+    NotFoundComponent,
+    LoginComponent,
+    FooterComponent,
+    RegistroComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
