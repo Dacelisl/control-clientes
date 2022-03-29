fc0f789a93f54d7e4b9269c6be889a9679297a91
Dacelisl <hero055@gmail.com>
2022-03-29 10:16:59 -0500

    config: header, footer, notFound, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { EditarComponent } from './components/editar/editar.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { TableroComponent } from './components/tablero/tablero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TableroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'configuracion', component: ConfiguracionComponent },
+  { path: 'cliente/editar/:id', component: EditarComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
